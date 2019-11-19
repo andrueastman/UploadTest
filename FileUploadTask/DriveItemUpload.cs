300df2bd4f557c2fe2ed772541e73b5c8749c54c
Andrew Omondi <v-anomon@microsoft.com>
2019-11-19 10:30:00 +0300

    Updates to fileupload sample

---
+++ FileUploadTask/DriveItemUpload.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileUploadTask
+{
+    public class DriveItemUpload
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="graphClient"></param>
+        /// <param name="itemId"></param>
+        /// <returns></returns>
+        public static async Task<IUploadSession> CreateDriveItemUploadSession(IBaseClient graphClient, string itemId)
+        {
+            // POST /v1.0/drive/items/01KGPRHTV6Y2GOVW7725BZO354PWSELRRZ:/SWEBOKv3.pdf:/microsoft.graph.createUploadSession
+            string uri = $"https://graph.microsoft.com/v1.0/drive/items/{itemId}:/SWEBOKv3.pdf:/microsoft.graph.createUploadSession";
+
+            HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, uri);
+            await graphClient.AuthenticationProvider.AuthenticateRequestAsync(httpRequestMessage);
+
+            // Read the session info from the response
+            var httpResponseMessage = await graphClient.HttpProvider.SendAsync(httpRequestMessage);
+            var content = await httpResponseMessage.Content.ReadAsStringAsync();
+            var uploadSession = graphClient.HttpProvider.Serializer.DeserializeObject<UploadSession>(content);
+
+            Console.WriteLine("Upload Session Created");
+
+            return uploadSession;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="graphClient"></param>
+        /// <param name="itemId"></param>
+        /// <returns></returns>
+        public static async Task UploadLargeFileInSlices(IBaseClient graphClient, string itemId)
+        {
+            
+            using Stream stream = Program.GetFileStream();
+
+            // Create upload session 
+            var uploadSession = await CreateDriveItemUploadSession(graphClient, itemId);
+
+            // Create task
+            var maxSliceSize = 320 * 1024; // 320 KB - Change this to your chunk size. 5MB is the default.
+            LargeFileUploadTask<DriveItem> largeFileUploadTask = new LargeFileUploadTask<DriveItem>(uploadSession, graphClient, stream, maxSliceSize);
+
+            // Setup the chunk request necessities
+            var slicesRequests = largeFileUploadTask.GetUploadSliceRequests();
+            var trackedExceptions = new List<Exception>();
+            DriveItem itemResult = null;
+
+            try
+            {
+                //upload the chunks
+                foreach (var request in slicesRequests)
+                {
+                    // Send chunk request
+                    var result = await largeFileUploadTask.UploadSliceAsync(request, trackedExceptions);
+                    // Do your updates here: update progress bar, etc.
+                    Console.WriteLine($"File uploading in progress. {request.RangeEnd} of {stream.Length} bytes uploaded");
+
+                    if (result.UploadSucceeded)
+                    {
+                        itemResult = result.ItemResponse;
+                        Console.WriteLine($"File uploading complete");
+                    }
+                }
+            }
+            catch (ServiceException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+        /// <summary>
+        /// Upload a large file using callbacks
+        /// </summary>
+        /// <param name="graphClient">Client for upload</param>
+        /// <param name="itemId">itemId for upload</param>
+        /// <returns></returns>
+        public static async Task UploadLargeFileWithCallBacks(IBaseClient graphClient, string itemId)
+        {
+
+            using Stream stream = Program.GetFileStream();
+
+            // POST /v1.0/drive/items/01KGPRHTV6Y2GOVW7725BZO354PWSELRRZ:/SWEBOKv3.pdf:/microsoft.graph.createUploadSession
+            var uploadSession = await CreateDriveItemUploadSession(graphClient,itemId);
+
+            // Create task
+            var maxSliceSize = 320 * 1024; // 320 KB - Change this to your chunk size. 5MB is the default.
+            LargeFileUploadTask<DriveItem> largeFileUploadTask = new LargeFileUploadTask<DriveItem>(uploadSession, graphClient, stream, maxSliceSize);
+
+            // Setup the progress monitoring
+            IProgress<long> progress = new Progress<long>(progress =>
+            {
+                Console.WriteLine($"Uploaded {progress} bytes of {stream.Length} bytes");
+            });
+
+            UploadResult<DriveItem> uploadResult = null;
+            try
+            {
+                uploadResult = await largeFileUploadTask.UploadAsync(progress);
+                    
+                if (uploadResult.UploadSucceeded)
+                {
+                    Console.WriteLine($"File Uploaded {uploadResult.ItemResponse.Id}");//Sucessful Upload
+                }
+            }
+            catch (ServiceException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
---
+++ FileUploadTask/FileAttachmentUpload.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileUploadTask
+{
+    public class FileAttachmentUpload
+    {
+        private static async Task<IUploadSession> CreateFileAttachementUploadSession(IBaseClient graphClient, string messageId, long streamLength)
+        {
+            // Create upload session only works on beta endpoint for now
+            // POST /beta/me/messages/{message-id}/attachments/createUploadSession
+            string uri = $"https://graph.microsoft.com/beta/me/messages/{messageId}/attachments/createUploadSession";
+            string attachementInfo = $"{{\"AttachmentItem\": {{\"attachmentType\": \"file\",\"name\": \"flower\",\"size\": {streamLength} }}}}";
+            HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, uri)
+            {
+                Content = new StringContent(attachementInfo, Encoding.UTF8, "application/json")
+            };
+            await graphClient.AuthenticationProvider.AuthenticateRequestAsync(httpRequestMessage);
+
+            // Read the session info from the response
+            var httpResponseMessage = await graphClient.HttpProvider.SendAsync(httpRequestMessage);
+            var content = await httpResponseMessage.Content.ReadAsStringAsync();
+            var uploadSession = graphClient.HttpProvider.Serializer.DeserializeObject<UploadSession>(content);
+
+            return uploadSession;
+        }
+        public static async Task UploadLargeAttachmentInSlices(IBaseClient graphClient, string messageId)
+        {
+            using Stream stream = Program.GetFileStream();
+
+            var uploadSession = await CreateFileAttachementUploadSession(graphClient, messageId, stream.Length);
+
+            // Create task
+            var maxSliceSize = 320 * 1024; // 320 KB - Change this to your slice size.
+
+            LargeFileUploadTask<FileAttachment> largeFileUploadTask = new LargeFileUploadTask<FileAttachment>(uploadSession, graphClient, stream, maxSliceSize);
+
+            // Setup the chunk request necessities
+            var slicesRequests = largeFileUploadTask.GetUploadSliceRequests();
+            var trackedExceptions = new List<Exception>();
+
+            Uri attachmentLocation = null;
+
+            //upload the chunks
+            foreach (var request in slicesRequests)
+            {
+                // Send chunk request
+                var result = await largeFileUploadTask.UploadSliceAsync(request, trackedExceptions);
+                // Do your updates here: update progress bar, etc.
+                Console.WriteLine($"File uploading in progress. {request.RangeEnd} of {stream.Length} bytes uploaded");
+
+                if (result.UploadSucceeded)
+                {
+                    attachmentLocation = result.Location;
+                    Console.WriteLine($"File uploading complete at : " + attachmentLocation.AbsoluteUri);
+                }
+            }
+
+            // Check that upload succeeded
+            if (attachmentLocation == null)
+            {
+                //Upload failed
+                Console.WriteLine("Upload failed");
+            }
+        }
+
+        public static async Task UploadLargeWithCallBack(IBaseClient graphClient, string messageId)
+        {
+
+            using Stream stream = Program.GetFileStream();
+
+            var uploadSession = await CreateFileAttachementUploadSession(graphClient, messageId,stream.Length);
+
+            // Create task
+            var maxSliceSize = 320 * 1024; // 320 KB - Change this to your slice size.
+            LargeFileUploadTask<FileAttachment> largeFileUploadTask = new LargeFileUploadTask<FileAttachment>(uploadSession, graphClient, stream, maxSliceSize);
+
+            // Setup the progress mechanism
+            IProgress<long> progress = new Progress<long>(progress =>
+            {
+                Console.WriteLine($"Uploaded {progress} bytes of {stream.Length} bytes");
+            });
+
+            UploadResult<FileAttachment> uploadResult = null;
+            try
+            {
+                uploadResult = await largeFileUploadTask.UploadAsync(progress);
+                if (uploadResult.UploadSucceeded)
+                {
+                    Console.WriteLine(uploadResult.Location);
+                }
+            }
+            catch (ServiceException e)
+            {
+                //try to refresh the upload info and resume the upload from where we left off.
+                Console.WriteLine("Something went wrong with the upload");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
