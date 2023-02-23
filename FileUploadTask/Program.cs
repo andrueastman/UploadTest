2d827017d36ce12492f44a0178e56e5df0991799
Andrew Omondi <andrew.omondi@microsoft.com>
2023-02-23 15:15:52 +0300

    Update console app

--- FileUploadTask/Program.cs
+++ FileUploadTask/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.Graph;
 using Microsoft.Identity.Client;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -9,17 +10,11 @@ namespace FileUploadTask
 {
     class Program
     {
+        const string clientID = "11e584da-5b80-404b-9a3c-0d510c105513"; // APP client id for app
+        const string filesDirectory = "C:\\Users\\user2\\Downloads\\BigFiles\\"; // Location of a large number of large files
+
         static async Task Main()
         {
-            // get a itemid from the /drive/root/children endpoint using graph explorer
-            string driveItemId = "DRIVE ITEM ID";
-
-            // get a message from the /me/messages endpoint using graph explorer
-            string messageId = "MESSAGE ID";
-
-            // APP client for app
-            string clientID = "APP ID FOR APPlICATION";
-
             /* Do the auth stuff first */
             IPublicClientApplication publicClientApplication = PublicClientApplicationBuilder
                 .Create(clientID)
@@ -37,15 +32,68 @@ namespace FileUploadTask
                 return Task.FromResult(0);
             });
 
+            var graphClient = new GraphServiceClient(delegatingAuthProvider);
 
-            var graphClient = new BaseClient("https://graph.microsoft.com/v1.0/", delegatingAuthProvider);
+            for (int i =0; i< 20; i++)
+            {
+                using var stream = GetFileStream();
+                await UploadFile(graphClient, "testFile.txt", stream);
+                Console.WriteLine($"Test {i+1} completed");
+            }
+        }
 
-            Console.WriteLine("Uploading large drive item file with callbacks");
-            await DriveItemUpload.UploadLargeFileWithCallBacks(graphClient, driveItemId);
+        public static async Task UploadFile(GraphServiceClient graphClient, string itemPath, Stream fileStream)
+        {
+            // Use properties to specify the conflict behavior
+            // in this case, replace
+            var uploadProps = new DriveItemUploadableProperties
+            {
+                ODataType = null,
+                AdditionalData = new Dictionary<string, object>
+                {
+                    { "@microsoft.graph.conflictBehavior", "replace" }
+                }
+            };
 
-            Console.WriteLine("Uploading large attachement file with callbacks");
-            await FileAttachmentUpload.UploadLargeAttachmentWithCallBack(graphClient, messageId);
+            // Create the upload session
+            // itemPath does not need to be a path to an existing item
+            var uploadSession = await graphClient.Me.Drive.Root
+                .ItemWithPath(itemPath)
+                .CreateUploadSession(uploadProps)
+                .Request()
+                .PostAsync();
 
+            // Max slice size must be a multiple of 320 KiB
+            int maxSliceSize = 320 * 1024 * 50;
+            var fileUploadTask =
+                new LargeFileUploadTask<DriveItem>(uploadSession, fileStream, maxSliceSize);
+
+            // Create a callback that is invoked after each slice is uploaded
+            IProgress<long> progress = new Progress<long>(prog =>
+            {
+                Console.WriteLine($"Uploaded {prog} bytes of {fileStream.Length} bytes");
+            });
+
+            try
+            {
+                // Upload the file
+                var uploadResult = await fileUploadTask.UploadAsync(progress);
+
+                if (uploadResult.UploadSucceeded)
+                {
+                    // The ItemResponse object in the result represents the
+                    // created item.
+                    Console.WriteLine($"Upload complete, item ID: {uploadResult.ItemResponse.Id}");
+                }
+                else
+                {
+                    Console.WriteLine("Upload failed");
+                }
+            }
+            catch (ServiceException ex)
+            {
+                Console.WriteLine($"Error uploading: {ex}");
+            }
         }
         
         /// <summary>
@@ -54,8 +102,11 @@ namespace FileUploadTask
         /// <returns></returns>
         public static Stream GetFileStream()
         {
-            string startupPath = Environment.CurrentDirectory;
-            FileStream fileStream = new FileStream(startupPath + "\\SWEBOKv3.pdf", FileMode.Open);
+            var rand = new Random(); 
+            var files = System.IO.Directory.GetFiles(filesDirectory, "*.*");
+            string nextFile = files[rand.Next(files.Length)];
+            FileStream fileStream = new(nextFile, FileMode.Open);
+            Console.WriteLine($"Picked file {nextFile} with size : {fileStream.Length}");
             return fileStream;
         }
     }
